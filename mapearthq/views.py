50304b09e2574bb3860a55ff0f8175ba6e343acd
Azzam <azzam.zafar21@gmail.com>
2022-12-27 12:33:07 +0530

    Feat: added folium maops to locate earthquake

--- mapearthq/views.py
+++ mapearthq/views.py
@@ -1,3 +1,4 @@
+import folium
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
 from . import models
@@ -8,5 +9,12 @@ from . import models
 
 def index(request):
     listings = models.WorldData.objects.all()
-        
-    return render(request,'index.html',{'listings': listings})
+    m = folium.Map([45.5236, -122.6750],zoom_start=3)
+    for listing in listings:
+        folium.CircleMarker([listing.lat,listing.lon],radius=2).add_to(m)    
+        folium.CircleMarker([listing.lat,listing.lon],radius=4).add_to(m)    
+        folium.CircleMarker([listing.lat,listing.lon],radius=8).add_to(m)    
+
+    m=m._repr_html_()
+    context = {'listings': listings,'world_map':m}
+    return render(request,'index.html',context)
