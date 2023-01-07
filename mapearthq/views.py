0337c16d079275cfcc5cce415aad041e85d88cb6
Azzam <azzam.zafar21@gmail.com>
2023-01-07 19:16:30 +0530

    fix:download csv;serving through a view

--- mapearthq/views.py
+++ mapearthq/views.py
@@ -1,16 +1,22 @@
 import folium
 from django.shortcuts import render
+from django.http import HttpResponse
 from mapearthq.models import WorldData, WeeklyCsvFile
 
 # Create your views here.
 
 # listings = models.WorldData.objects.all()
 # print(listings[0].country)
-
+def download_csv(request):
+    weekly_data = WeeklyCsvFile.objects.last()
+    file = weekly_data.csv_file.file
+    data = file.read().decode()
+    response = HttpResponse(data,content_type='text/plain')
+    response['Content-Disposition']='attachment; filename="last_week.csv"'
+    return response
 
 def index(request):
     listings = WorldData.objects.order_by("-time")[:30]
-    weekly_data = WeeklyCsvFile.objects.last()
     m = folium.Map(location=[listings[0].lat, listings[0].lon], zoom_start=2)
     for listing in listings:
         popupstr = f"{listing.country}\nMagnitude:{listing.Mag}"
@@ -56,5 +62,5 @@ def index(request):
 
     m = m._repr_html_()
     WeeklyCsvFile
-    context = {"listings": listings, "world_map": m, "csv_obj": weekly_data}
+    context = {"listings": listings, "world_map": m}
     return render(request, "index.html", context)
