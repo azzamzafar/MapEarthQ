1dc6b117bbec9d64b1f1b90c380ae63ba1880f94
Azzam <azzam.zafar21@gmail.com>
2023-01-08 18:55:46 +0530

    hostname,secretkey,no external db

--- Project/settings.py
+++ Project/settings.py
@@ -158,3 +158,7 @@ class Prod(Dev):
     SECRET_KEY = values.SecretValue()
     # security.W008
     SECURE_SSL_REDIRECT = True
+    ALLOWED_HOSTS = super().ALLOWED_HOSTS
+    RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+    if RENDER_EXTERNAL_HOSTNAME:
+        ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
